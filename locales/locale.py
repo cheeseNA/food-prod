db7f43504b15d6dac6a558f200d394b3131bf3c3
cheeseNA <kurocat2000@gmail.com>
2024-06-12 20:42:50 +0900

    Add locale module

---
+++ locales/locale.py
@@ -0,0 +1,18 @@
+import json
+
+
+def get_current_lang():
+    settings = json.load(open("locales/locale_settings.json", "r"))
+    return settings["lang"]
+
+
+def generate_localer(lang: str):
+    ja_to_en = json.load(open("locales/ja_to_en.json", "r"))
+
+    def localer(key: str):
+        if lang == "ja" or key not in ja_to_en:
+            return key
+        return ja_to_en[key]
+
+    return localer
+    return localer
