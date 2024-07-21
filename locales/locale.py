7bb115971c1731848f35e6ec219549551d3ff675
yamakata <yamakata@mi.u-tokyo.ac.jp>
2024-07-21 11:56:50 +0900

    specify charactoer code

--- locales/locale.py
+++ locales/locale.py
@@ -2,12 +2,12 @@ import json
 
 
 def get_current_lang():
-    settings = json.load(open("locales/locale_settings.json", "r"))
+    settings = json.load(open("locales/locale_settings.json", "r", encoding="utf-8"))
     return settings["lang"]
 
 
 def generate_localer(lang: str):
-    ja_to_en = json.load(open("locales/ja_to_en.json", "r"))
+    ja_to_en = json.load(open("locales/ja_to_en.json", "r", encoding="utf-8"))
 
     def localer(key: str):
         if lang == "ja" or key not in ja_to_en:
